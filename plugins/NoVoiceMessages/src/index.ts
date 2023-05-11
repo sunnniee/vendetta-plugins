7430468333484d7f8aec64ca006250b2638d2037
ActuallyTheSun <78964224+ActuallyTheSun@users.noreply.github.com>
2023-05-11 14:08:42 +0300

    update NoVoiceMessages

--- plugins/NoVoiceMessages/src/index.ts
+++ plugins/NoVoiceMessages/src/index.ts
@@ -1,7 +1,7 @@
 import { findByStoreName } from "@vendetta/metro"
 import { FluxDispatcher } from "@vendetta/metro/common";
 import { before, instead } from "@vendetta/patcher"
-import { storage } from "@vendetta/plugin";
+import { storage as _storage } from "@vendetta/plugin";
 import settings from "./settings.js";
 
 interface ActionHandler {
@@ -11,33 +11,32 @@ interface ActionHandler {
 
 const SEND_VOICE_MESSAGES = 1n << 46n;
 
-let patches = [] as (Function | Function[])[]
-const unpatch = () => {
-    patches.forEach(p => {
-        if (typeof p === "function") p()
-        else if (Array.isArray(p)) p.forEach(f => f())
-    })
-    const len = patches.length
-    for (let i = 0; i < len; i++) patches.pop()
-}
+export const settingsObj = {
+    voiceMessageButton: {
+       byDefault: true,
+        onEnable() {
+            const PermissionStore = findByStoreName("PermissionStore")
+            patches.voiceMessageButton.push(
+                instead("can", PermissionStore, (args: [bigint, ...unknown[]], fn) => {
+                    if(args[0] === SEND_VOICE_MESSAGES) return false;
+                    else return fn(...args)
+                })
+            )
+        },
+        onDisable() { unpatch("voiceMessageButton") }
+    },
 
-export default {
-  onLoad: () => {
-    const PermissionStore = findByStoreName("PermissionStore")
-    const { voiceMessageButton, voiceMessages } = storage;
-    patches.push(
-        (voiceMessageButton ?? true) && instead("can", PermissionStore, (args: [bigint, ...unknown[]], fn) => {
-            if(args[0] === SEND_VOICE_MESSAGES) return false;
-            else return fn(...args)
-        }),
-        voiceMessages && ((() => {
+    voiceMessages: {
+        byDefault: false,
+        onEnable() {
             const handlers = FluxDispatcher._actionHandlers._orderedActionHandlers as {[event: string]: ActionHandler[]}
             const [LoadMessagesSuccess, MessageCreate, MessageUpdate] = Object.entries(handlers)
             .filter(([evName]) => ["LOAD_MESSAGES_SUCCESS", "MESSAGE_CREATE", "MESSAGE_UPDATE"].includes(evName))
             .map(([_, handlerArray]) => handlerArray.find(h => h.name === "MessageStore"))
-            return [
+
+            patches.voiceMessages.push(
                 before("actionHandler", LoadMessagesSuccess, (args) => {
-                    args[0].messages &&= args[0].messages.filter(m => m.attachments.every(a => !a.waveform))
+                    args[0].messages &&= args[0].messages.filter(m => m.attachments?.every(a => !a.waveform))
                 }),
                 before("actionHandler", MessageCreate, (args) => {
                     if (args[0].message?.attachments?.some(a => a.waveform))
@@ -47,9 +46,37 @@ export default {
                     if (args[0].message?.attachments?.some(a => a.waveform))
                         args[0].message = {}
                 }),
-            ]
-        })() as Function[]),
-    )
+            )
+        },
+        onDisable() { unpatch("voiceMessages") }
+    }
+} as const
+
+const patches: { [k in keyof typeof settingsObj]: Function[] } = {
+    voiceMessageButton: [],
+    voiceMessages: []
+}
+function unpatch(name?: keyof typeof patches) {
+    if (name) {
+        patches[name].forEach(fn => fn())
+        patches[name].splice(0, patches[name].length)
+    }
+    else Object.entries(patches).forEach(([n, fns]) => {
+        fns.forEach(fn => fn())
+        patches[n].splice(0, patches[n].length)
+    })
+}
+
+Object.entries(settingsObj).forEach(([settingName, { byDefault }]) => {
+    if (!(settingName in _storage)) _storage[settingName] = byDefault
+})
+
+export default {
+  onLoad: () => {
+    const storage = _storage as { [k in keyof typeof settingsObj]: boolean }
+    Object.entries(settingsObj).forEach(([setting, info]) => {
+        if (storage[setting]) info.onEnable()
+    })
   },
   onUnload: unpatch,
   settings
--- plugins/NoVoiceMessages/src/settings.tsx
+++ plugins/NoVoiceMessages/src/settings.tsx
@@ -1,28 +1,34 @@
 import { ReactNative } from "@vendetta/metro/common";
-import { Forms } from "@vendetta/ui/components";
+import { ErrorBoundary, Forms } from "@vendetta/ui/components";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
+import { settingsObj } from "./index.js";
+
+function onToggle(name: keyof typeof settingsObj, value: boolean) {
+    storage ?? {}
+    storage[name] = value;
+    if (value) settingsObj[name].onEnable()
+    else settingsObj[name].onDisable()
+}
 
 export default () => {
     useProxy(storage);
 
     return (
+    <ErrorBoundary>
         <ReactNative.ScrollView>
             <Forms.FormSwitchRow
                 label="Remove voice message button from chat"
-                subLabel="Restart plugin to apply (for both settings)"
-                onValueChange={(v) => {
-                    storage.voiceMessageButton = v
-                }}
-                value={storage.voiceMessageButton ?? true}
+                onValueChange={(v) => onToggle("voiceMessageButton", v)}
+                value={storage.voiceMessageButton}
             />
             <Forms.FormSwitchRow
                 label="Remove voice messages altogether"
-                onValueChange={(v) => {
-                    storage.voiceMessages = v
-                }}
-                value={storage.voiceMessages ?? false}
+                subLabel="Requires restart to remove already received voice messages"
+                onValueChange={(v) => onToggle("voiceMessages", v)}
+                value={storage.voiceMessages}
             />
         </ReactNative.ScrollView>
+    </ErrorBoundary>
     );
 };
